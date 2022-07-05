fe6cf6ff5ef071a715e1db9a728edb4d2b9008df
qhtkdrma <kimwonkyu15@gmail.com>
2022-07-06 00:03:47 +0900

    회원정보 수정 중 닉네임 수정 완료

--- routes/page.js
+++ routes/page.js
@@ -16,6 +16,11 @@ router.get('/profile', isLoggedIn, (req, res) => {
   res.render('profile', { title: '내 정보 - NodeBird' });
 });
 
+router.get('/editinfo', isLoggedIn, (req, res) => {
+  res.render('editinfo', { title: '개인정보 수정' });
+});
+
+
 router.get('/join', isNotLoggedIn, (req, res) => {
   res.render('join', { title: '회원가입 - NodeBird' });
 });
